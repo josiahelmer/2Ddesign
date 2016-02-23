27b03fc4c23c010ea9b4c7408b182416847b9f5d
josiahelmer <jelm9149@740s-pl-112.local>
2016-02-23 11:45:27 -0700

    adding object to controller

--- src/design/controller/TwoDController.java
+++ src/design/controller/TwoDController.java
@@ -2,10 +2,19 @@ package design.controller;
  
 import design.model.*;
 import design.view.TwoDFrame;
+import java.awt.Color;
 
 public class TwoDController
 {
 	private TwoDFrame baseFrame;
+	private BasketBall [][] myBasketBall;
+	
+	public TwoDController()
+	{
+		myBasketBall = new BasketBall[3][3];
+		setupArray();
+		baseFrame = new TwoDFrame(this);
+	}
 	
 	public void start()
 	{
